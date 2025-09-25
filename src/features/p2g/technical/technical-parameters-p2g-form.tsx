de4398632caad45b08f4be6394566fa01aeb0a51
Maksim Fetisov <maksim.fetisov@nfq.com>
2025-09-25 10:50:10 +0200

    update p2g fields title/description

--- src/features/p2g/technical/technical-parameters-p2g-form.tsx
+++ src/features/p2g/technical/technical-parameters-p2g-form.tsx
@@ -10,6 +10,74 @@ import { FormNavigation } from "@/components/navigation/form-navigation"
 import Divider from "@mui/material/Divider"
 import { ReactionTimeSlider } from "@/components/reaction-time-slider"
 import { Title } from "@/ui/title"
+import { useWatch } from "react-hook-form"
+import { InfoBanner } from "@/components/infoBanner/InfoBanner.tsx"
+import { Accordion } from "@/ui/accordion"
+
+const FormContent = ({ handleBackward }: { handleBackward: () => void }) => {
+    const serviceType = useWatch({ name: "service_type" })
+
+    return (
+        <>
+            <Stack spacing={3}>
+                <Title style={{ fontSize: "32px", marginBottom: "48px", fontWeight: 400 }}>Techniniai parametrai</Title>
+
+                <FormInput
+                    name="Q_max"
+                    title="Maksimali įrenginio galia (MW)"
+                    type="number"
+                    description="Įvedami tik teigiami skaičiai, maksimali reikšmė 1 000 000 MW (1 TW)"
+                />
+
+                <Divider />
+
+                <ServiceTypeSelect />
+
+                {(serviceType === "down" || serviceType === "both") && (
+                    <ReactionTimeSlider
+                        field={"reaction_time_d"}
+                        label={"Reakcijos laikas įjungiant elektrolizerį (reguliavimas žemyn) "}
+                    />
+                )}
+                {(serviceType === "up" || serviceType === "both") && (
+                    <ReactionTimeSlider
+                        field={"reaction_time_u"}
+                        label={"Reakcijos laikas išjungiant elektrolizerį (reguliavimas aukštyn)"}
+                    />
+                )}
+            </Stack>
+
+            <Stack spacing={2}>
+                <FormInput name="eta_H2" title="Vandenilio kaina (EUR/kg)" type="number" />
+                <FormInput
+                    name="electrolyzer_tech"
+                    title="Elektrolizerio (elektra → vandenilis) naudingumo koeficientas (%)"
+                />
+
+                <Accordion
+                    title="Išplėstiniai techniniai parametrai"
+                    titleDescription={
+                        <InfoBanner
+                            title=""
+                            subtitle="Patikslinkite savo įrenginio technines charakteristikas, 
+                        kad skaičiavimai būtų kuo tikslesni."
+                            description="Atkreipiame dėmesį, kad šios informacijos pateikimui, 
+                        geriausia pasitelkti įrenginio technines specifikacijas. Jei kažkurių 
+                        verčių nežinote, nesijaudinkite, parinkome numatytąsias reikšmes, kurios atspindi 
+                        rinkoje esančių įrenginių galimybes."
+                        />
+                    }
+                >
+                    <FormInput name="T0" title="T0" type="number" />
+                    <FormInput name="p0" title="p0" type="number" />
+                    <FormInput name="eta_C" title="eta_C" type="number" />
+                </Accordion>
+            </Stack>
+
+            <FormNavigation handleBackward={handleBackward} />
+        </>
+    )
+}
 
 export const TechnicalParametersP2gForm = () => {
     const navigate = useNavigate()
@@ -35,30 +103,7 @@ export const TechnicalParametersP2gForm = () => {
             validationSchema={technicalParametersP2gSchema}
             defaultValues={location.state?.technicalParameters?.p2g || defaultTechnicalParametersP2g}
         >
-            <Stack spacing={3}>
-                <Title style={{fontSize: '32px', marginBottom: '48px', fontWeight: 400}} >Techniniai parametrai</Title>
-                
-                <FormInput name="Q_max" title="Maksimali įrenginio galia (MW)" type="number" />
-
-                <Divider />
-
-                <ServiceTypeSelect />
-
-                <ReactionTimeSlider field={"reaction_time_d"} label={"Reakcijos laikas įjungiant elektrolizerį (reguliavimas žemyn) "} />
-
-                <ReactionTimeSlider field={"reaction_time_u"} label={"Reakcijos laikas išjungiant elektrolizerį (reguliavimas aukštyn)"} />
-            </Stack>
-
-            <Stack spacing={2}>
-                <FormInput name="eta_H2" title="eta_H2" type="number" />
-                <FormInput name="electrolyzer_tech" title="electrolyzer_tech" />
-
-                <FormInput name="T0" title="T0" type="number" />
-                <FormInput name="p0" title="p0" type="number" />
-                <FormInput name="eta_C" title="eta_C" type="number" />
-            </Stack>
-
-            <FormNavigation handleBackward={handleBackward} />
+            <FormContent handleBackward={handleBackward} />
         </Form>
     )
 }
