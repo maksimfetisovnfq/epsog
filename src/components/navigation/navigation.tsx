5deaf0baa78b7a5af66793cb9d385e7994e47953
Maksim Fetisov <maksim.fetisov@nfq.com>
2025-10-01 09:38:21 +0200

    add Litgrid logo

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -30,32 +30,39 @@ const isPageAccessible = (pagePath: string, state: HistoryState): boolean => {
 export const Navigation = () => {
     const location = useLocation();
     const calculatorType = location.pathname.split('/')[1];
-    
-    
+
     return (
-        <Box className="p-2 flex gap-2" sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "space-between",
-            width: {sm: 768},
-        }}>
-            {links.map(link => {
-                const isHome = location.pathname === '/';
-                const isGeneralData = link.to === '/general-data';
-                const isAccessible = isGeneralData && isHome
-                    ? false
-                    : isPageAccessible(link.to, location.state);
-                return (
-                    <NavLink
-                        key={link.to}
-                        to={link.to === '/general-data' ? link.to : `/${calculatorType}${link.to}`}
-                        state={location.state}
-                        disabled={!isAccessible}
-                        isHome={isHome}
-                    >  {link.label}
-                    </NavLink>
-                );
-            })}
-        </Box>
+        <>
+            {location.pathname === '/' && (
+                <Box sx={{ justifyContent: "space-between", display: "flex", alignItems: "center", marginBottom: "20px" }}>
+                    <img src="/Litgrid-logo-horizontalus-baltas-RGB.png" style={{ width: 120 }} alt="" />
+                    <img src="/EPSOG_logo_WHITE.png" style={{ width: 100, height: 30}} alt="" />
+                </Box>
+            )}
+            <Box className="p-2 flex gap-2" sx={{
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: "space-between",
+                width: { sm: 779 },
+            }}>
+                {links.map(link => {
+                    const isHome = location.pathname === '/';
+                    const isGeneralData = link.to === '/general-data';
+                    const isAccessible = isGeneralData && isHome
+                        ? false
+                        : isPageAccessible(link.to, location.state);
+                    return (
+                        <NavLink
+                            key={link.to}
+                            to={link.to === '/general-data' ? link.to : `/${calculatorType}${link.to}`}
+                            state={location.state}
+                            disabled={!isAccessible}
+                            isHome={isHome}
+                        >  {link.label}
+                        </NavLink>
+                    );
+                })}
+            </Box>
+        </>
     );
 };
